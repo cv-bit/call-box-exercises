4e6c7c9610dd20a969ee64cd6c2bf7f7b4c795b9
cv-bit <ververcameron29@gmail.com>
2024-03-20 04:10:15 -0500

    adding javascript to show and hide dropdown menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
-console.log('hello world')
+// classic hello world to make sure my JavaScript is properly linked
+console.log('hello world');
 
-// here I am grabbing the html element to manipulate
+// here I am grabbing the html elements to manipulate
 const inventoryBtn = $("#inventory-btn");
+const dropDownMenu = $("#dropdown-menu");
+const dropDownLinks = $(".dropdown-link");
 
 // here I am creating a state I can track
-let isMenuOpen = false
+let isMenuOpen = false;
+
+// adding event listener to each link to close menu
+dropDownLinks.each(function (i) {
+    dropDownLinks[i].addEventListener("click", function () {
+        isMenuOpen = false
+        dropDownMenu.addClass('dropdown-menu-hide').removeClass("dropdown-menu-show")
+    })
+})
 
 // here I am adding a click event 
 inventoryBtn.on("click", function () {
-    // on menu click first thing I want to do is set menu open to the opposite of what it was
-    isMenuOpen = !isMenuOpen
-    console.log('clicked', isMenuOpen)
-})
+    // on menu click first thing I want to do is set isMenuOpen to the opposite of what it was
+    isMenuOpen = !isMenuOpen;
+    // then depending on the state of isMenuOpen either show or hide element
+    if(isMenuOpen) {
+        dropDownMenu.addClass('dropdown-menu-show').removeClass("dropdown-menu-hide")
+    } else if(!isMenuOpen) {
+        dropDownMenu.addClass('dropdown-menu-hide').removeClass("dropdown-menu-show")
+    }
+
+});
